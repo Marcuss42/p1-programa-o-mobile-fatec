eebbca8f427cb49a7b8e7d79a6680c0f11b0e296
Marcus <mpfrancisco2003@gmail.com>
2025-09-25 21:59:34 -0300

    feat(p1): cria componente principal e faz hello

---
+++ gerenciador-lembretes/src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1> Hello, Lembretes </h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
