0c867eb731e5aa36fb3383df998a3af4c17e707d
Marcus <mpfrancisco2003@gmail.com>
2025-09-25 23:46:14 -0300

    feat(p1): cria componente de entrada

--- gerenciador-lembretes/src/App.jsx
+++ gerenciador-lembretes/src/App.jsx
@@ -1,15 +1,42 @@
+import React from "react";
+import LembreteEntrada from "./LembreteEntrada";
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      vetorLembretes: []
+    };
+  }
 
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <h1> Hello, Lembretes </h1>
+  adicionarLembrete = (descricao) => {
+
+    this.state.vetorLembretes.push({
+      descricao: descricao,
+      favorito: false
+    });
+
+    console.log(this.state.vetorLembretes);
+
+    this.setState({ vetorLembretes: this.state.vetorLembretes }); 
+  }
+
+  render() {
+    const textoBotao = "Cadastrar Lembrete";
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <LembreteEntrada
+              textoBotao={textoBotao}
+              funcAdicionar={this.adicionarLembrete}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  )
+    );
+  }
 }
 
-export default App
+export default App;
